88238f6ed143cd3bda4587ae3561ec000c00ae03
Daniel Chavez <danstrike@hotmail.com>
2024-11-19 00:16:47 -0600

    Initial commit

---
+++ src/test/java/com/utm/sas/SchoolAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utm.sas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
